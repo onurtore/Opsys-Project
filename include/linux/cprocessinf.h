0764dda3fb0a55c386dae262f662bb795cf8a8f7
jesst <onurberk.tore@std.yeditepe.edu.tr>
2018-03-04 02:22:02 +0300

    Add some fix

--- include/linux/cprocessinf.h
+++ include/linux/cprocessinf.h
@@ -1,4 +1,14 @@
 #include <linux/unistd.h>
 #include <errno.h>
+
 extern int errno;
-_syscall3(int, cprocessinf, struct, prdata *data, int, option, long, nicev);
+
+struct prcdata{
+	long prio; /* calculated with (20-process'es nice value) */
+	long weight; /* calculated with process'es counter value + prio */
+	pid_t pid; /* process id*/
+	pid_t pidparent; /* process id of parent process*/
+	int processcount; /* number of process of owner of current process. */
+};
+
+_syscall3(int, cprocessinf, struct prcdata *,data, int, option, long, nicev);
